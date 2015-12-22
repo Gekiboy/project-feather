7290fbbbc79fc40701a839acf08c124bb0a9d1e5
Lochlan Wansbrough <lochie@live.com>
2015-12-22 11:08:02 -0800

    Added watch tasks for objects and textures

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -5,7 +5,9 @@ gulp.task('watch', function() {
 	gulp.watch('./src/js/**', ['browserify']);
 	gulp.watch('./src/images/**', ['images']);
 	gulp.watch('./src/fonts/**', ['fonts']);
+	gulp.watch('./src/objects/**', ['objects']);  
 	gulp.watch('./src/scss/**', ['styles']);
+	gulp.watch('./src/textures/**', ['textures']);  
 	gulp.watch('./src/index.html', ['html']);
   
   gulp.watch(['./dist/**']).on('change', function(file) {
